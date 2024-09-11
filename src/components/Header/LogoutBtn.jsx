2ec7561d8506f60589342b1cb2e828e8e5cbdd00
jayparve <jayparve44@gmail.com>
2024-09-11 16:14:43 +0530

    logout btn problem

--- src/components/Header/LogoutBtn.jsx
+++ src/components/Header/LogoutBtn.jsx
@@ -7,11 +7,26 @@ import {logout} from "../../store/authSlice"
 function LogoutBtn() {
   const dispatch = useDispatch()
 
-  const logouthandler = ()=>{
-    authService.logout().then(()=>{
-      dispatch(logout())
-    })
+  const logouthandler = () => {
+    if (!authService) {
+      console.error('AuthService is not defined')
+      return
+    }
+
+    authService
+      .logout()
+      .then(() => {
+        if (dispatch) {
+          dispatch(logout())
+        } else {
+          console.error('Cannot dispatch logout action')
+        }
+      })
+      .catch((error) => {
+        console.error('Error logging out:', error)
+      })
   }
+
   return (
     <Link onClick={logouthandler} to="/" className="bg-white text-purple-600 px-4 py-2 rounded-md hover:bg-purple-100 transition duration-300">
       Logout →
